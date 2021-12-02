bb9c584d752bee50fd9f879099733461599b0b36
ReneGliddon <37896877+ReneGliddon@users.noreply.github.com>
2021-12-01 22:30:58 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,81 +1,76 @@
 #include <iostream>
-#include "myStack.h"
+#include <stack>
 #include <string>
 using namespace std;
-bool isInLanguageL(string w)
-{
-    cout<<"here";
-    stackType<char> s;
-    int index = 0;
-   // bool inLang;
 
-    if(w[index]=='a')
-    {
-        index++;
-        cout<<endl<<"a";
-    }
+// program to test if a word belongs to the language of n+a a's, followed by n b's using a stack
 
-
-    while(w[index]=='a')
-    {
-        s.push('X');
-        index++;
-        cout<<endl<<"more a";
-    }
-
-    while(w[index]=='b')
-    {
-       // cout<<endl<<"b";
-        if (!s.isEmptyStack())
-        {
-            if(s.top()=='X')
-            {
-                s.pop();
-            }
-        }
-        else
-        {
-
-            return false;
-        }
-        index++;
-    }
-
-    cout<<"end";
-    return(index==w.size()&&s.isEmptyStack());
-
-}
-
+//function header
+bool isInLanguageL (string w);
 
 int main()
 {
+    //string to hold the word to be tested
+    string word;
 
- string a="aaaabbbbb";
- string b="aaababbbbb";
- string c="aaaabbbbba";
+    //request a word to be tested
+    cout<<"Enter a word and this program will check if it belongs in the language of n+1 a's, followed by n b's."<<endl;
 
- string s1, s2, s3;
- bool one = isInLanguageL(a);
- cout<<one;
- if(one)
-    s1 = " is ACCEPTED";
- else
-    s1= " is REJECTED";
+    //receive the word to be tested
+    cin>>word;
 
-bool two = isInLanguageL(b);
- if(two)
-    s2 =" is ACCEPTED";
- else
-    s2 =" is REJECTED";
+    //call isInLanguageL, if it returns true, then the word is in the language
+    if(isInLanguageL(word))
+    {
+        cout<<"The word is in the language."<<endl;
+    }
+    //if it returns false, then it is not in the language
+    else
+        cout<<"The word is not in the language."<<endl;
 
-  bool three = isInLanguageL(c);
- if(three)
-    s3 =" is ACCEPTED";
- else
-    s3 =" is REJECTED";
 
- cout << a + s1 <<endl;
- cout << b + s2 <<endl;
- cout << c + s3 <<endl;
- return 0;
+    return 0; //Line 21
 }
+
+bool isInLanguageL(string w)
+{
+    //initialize a new stack, myStack
+    stack<char> myStack;
+
+    //store the string as chars into the stack
+     string str = w;
+     char c[str.size() + 1];
+     str.copy(c, str.size() + 1);
+     for(int i = 0; i <str.size();i++)
+         myStack.push(c[i]);
+
+    //if the stack has an even number of chars, the word is not in the language
+    if((myStack.size()%2)==0)
+    {
+        return false;
+    }
+
+    //declare halfsize, a variable to hold half the size of the stack
+    int halfsize = myStack.size()/2;
+
+    //check that half the stack is comprised of b's
+    for(int i = 0; i < halfsize; i++)
+    {
+        if (myStack.top() == 'b')
+            myStack.pop();
+        else
+            //if it is comprised of anything else, then the word is not in the language
+            return false;
+    }
+    //check that the second half plus one more char, is comprised of a's
+    for(int i = 0; i <halfsize+1; i++)
+    {
+        if(myStack.top()=='a')
+            myStack.pop();
+        //if it is comprised of anything else, then the word is not in the language
+        else
+            return false;
+    }
+    //if we made it through the checks without returning false, then the word is in the language
+        return true;
+}//end isInLanguageL
